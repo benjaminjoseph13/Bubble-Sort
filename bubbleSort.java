2102c23e75b4cc459a62435520b0e88da2821435
benjaminjoseph13 <benjaminjosephlongakit@gmail.com>
2015-10-25 03:35:41 +0800

    Bubble Sort

---
+++ bubbleSort.java
@@ -0,0 +1,44 @@
+public class bubbleSort extends mainClass{
+
+    public bubbleSort(int[] number) {
+    }
+
+       public void Ascending(){
+        int tmp;
+        for(int a=0; a<number.length; a++){
+            for( int b =0; b<10-1; b++){
+                if(number[b] > number[b+1]){
+                    tmp = number[b];
+                    number[b] = number[b+1];
+                    number[b+1] = tmp;
+
+             }
+        }
+    }
+
+
+        System.out.print("ASCENDING: ");
+        for(int x=0;x<10; x++){
+            System.out.print(number[x] +", ");
+        }
+       }
+
+
+       public void Descending(){
+        int tmp1;
+        for(int j=0; j<number.length; j++){
+            for( int k =0; k<10-1; k++){
+                if(number[k] < number[k+1]){
+                    tmp1 = number[k];
+                    number[k] = number[k+1];
+                    number[k+1] = tmp1;
+             }
+        }
+    }
+        System.out.print("\nDESCENDING: ");
+        for(int x=0;x<10;x++){
+            System.out.print(number[x] +", ");
+    }
+}
+
+}
